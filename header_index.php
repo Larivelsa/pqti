7b363f17b1b30d2514665149f3c57ed8dd3105b1
Larivelsa <larissavelsa@gmail.com>
2021-07-21 11:15:39 -0300

    inicial

---
+++ header_index.php
@@ -0,0 +1,32 @@
+﻿<section id="cabecalhoTotal2">
+<div class="logo">
+<a href="index.php"><img src="img/logo.png" alt="logotipo PqTI" /></a>
+</div>
+
+
+<div class="cabecalhoConteudo">
+<nav id="menu">
+<ul>
+
+	<li><a href="pages/manutencoes.php">MANUTENÇÕES</a></li>
+	<li><a href="pages/cadastros.php">GERENCIAR</a></li>
+	<li><a href="pages/estatisticas.php">ESTATÍSTICAS</a></li>
+	<li><a href="pages/#" onclick="return alert('Se o equipamento não possuir bem patrimonial, utilize o número de série (identificador único) até que seja emplacado.')">AJUDA</a></li>
+	<li><a href="pages/logout.php">SAIR</a></li>
+
+</ul>
+
+</nav>
+
+</div>
+</section>
+
+<?php
+include "pages/verifica_sessao_usuario.php";
+?>
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,56 @@
+﻿<?php
+include "function/mysqlconecta.php";
+include "function/mysqlexecuta.php";
+
+
+            session_start("login_usuario");
+            
+            if(empty($_SESSION['email'])){
+                header("location: pages/login.php");
+            }
+			$nome=$_SESSION['nome'];
+        ?>
+
+<!doctype html>
+<head>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+<title>Bem-vindo ao PqTI</title>
+<link href = "css/style.css" rel = "stylesheet" type="text/css">
+<script src="js/script.js" type = "text/javascript"></script>
+</head>
+
+
+	
+<body>
+
+	<header>
+	<?php
+	include "header_index.php";
+	?>
+		
+	</header>
+	
+	<section id = "conteudo">
+	<label>Olá, <?php echo $nome;?>!</label><br><br>
+	<fieldset>
+	<legend>Acesso direto</legend>
+	<ul>
+	<li><a href="pages/cadastra_manutencaoInternaPC.php">REGISTRAR MANUTENÇÃO INTERNA EM COMPUTADOR</a></li>
+	<li><a href="pages/cadastra_manutencaoInternaImpressora.php">REGISTRAR MANUTENÇÃO EM IMPRESSORA</a></li>
+	<li><a href="pages/cadastra_manutencaoInternaOutros.php">REGISTRAR MANUTENÇÃO INTERNA GERAL</a></li>
+	<li><a href="pages/cadastra_manutencaoImpressora.php">REGISTRAR MANUTENÇÃO EXTERNA IMPRESSORA</a></li>
+	<li><a href="pages/cadastra_manutencaoExternaOutros.php">REGISTRAR MANUTENÇÃO EXTERNA GERAL</a></li>
+	</ul>
+	</fieldset>
+	</section>
+
+	<footer>
+	<?php
+	//include "pages/rodape.php";
+	?>
+	</footer>
+</body>
+	
+	
+</html>
